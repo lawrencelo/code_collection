363177d34efea16819b2af7541ba5d7c312d1f0a
Lawrence Lo <lawrence@datainsightsolutions.com>
2017-09-13 16:27:53 -0400

    add staircase.py

---
+++ hankerrank/Algorithms/Warmup/staircase.py
@@ -0,0 +1,12 @@
+import sys
+
+
+n = int(input().strip())
+
+for i in range(0,n):
+    for j in range(0,n):
+        if i+j < n-1:
+            print(" ",end='')
+        else:
+            print("#",end='')
+    print()
