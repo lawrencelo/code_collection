c8f36839f6e8dc6dcaf6d1f39e1efe1c08bff3fd
Lawrence Lo <lawrence@datainsightsolutions.com>
2017-09-19 22:49:11 -0400

    shortest and longest CITY names

---
+++ hankerrank/SQL/10select.sql
@@ -0,0 +1,6 @@
+/*
+Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
+*/
+
+SELECT city, LENGTH(city) from STATION GROUP BY city ORDER BY LENGTH(city), city ASC LIMIT 1;
+SELECT city, LENGTH(city) from STATION GROUP BY city ORDER BY LENGTH(city) DESC LIMIT 1;
